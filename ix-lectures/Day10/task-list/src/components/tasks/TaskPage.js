bc819921f110cd96bf929c55abd4c1447162f1cf
swyx <michelle.shelfish@gmail.com>
2022-07-22 17:58:55 +0200

    Day 10 added

---
+++ ix-lectures/Day10/task-list/src/components/tasks/TaskPage.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+
+import TaskInput from './TaskInput';
+import TaskTable from './TaskTable';
+
+import taskService from '../../services/task.service';
+
+export default function TaskPage() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  async function fetchTasks() {
+    try {
+      const tasks = await taskService.readTasks();
+      setTasks(tasks);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function onTaskCreated(task) {
+    try {
+      task = await taskService.createTask(task);
+
+      // update the tasks state with the new task
+      const newTasks = [...tasks, task];
+      setTasks(newTasks);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function onTaskUpdated(task) {
+    try {
+      task = await taskService.updateTask(task);
+
+      const newTasks = tasks.map((t) => {
+        return t.id === task.id ? task : t;
+      });
+      setTasks(newTasks);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function onTaskRemove(task) {
+    try {
+      await taskService.deleteTask(task);
+
+      const newTasks = tasks.filter((t) => {
+        return t.id !== task.id;
+      });
+      setTasks(newTasks);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div className='container mt-5'>
+      <div className='card card-body text-center'>
+        <h1>Task List</h1>
+        <hr></hr>
+        <div>
+          Our Simple Task List
+        </div>
+
+        <TaskInput onTaskCreated={onTaskCreated} />
+
+        <TaskTable
+          tasks={tasks}
+          onTaskUpdated={onTaskUpdated}
+          onTaskRemove={onTaskRemove}
+        />
+
+      </div>
+    </div>
+  )
+}
---
+++ ix-lectures/Day10/task-list/src/models/task.js
@@ -0,0 +1,12 @@
+
+export class Task {
+  constructor(
+    id,
+    name,
+    complete,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.complete = complete;
+  }
+}
